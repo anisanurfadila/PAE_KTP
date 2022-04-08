09b0c096b3f754db695bcd49ad6931faa83dd526
anisanurfadila <anisanfd925@gmail.com>
2022-04-08 14:08:38 +0700

    Commit 1 / 8 Mar

---
+++ src/main/java/pae/umy/projectktp2/Projectktp2Application.java
@@ -0,0 +1,13 @@
+package pae.umy.projectktp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projectktp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projectktp2Application.class, args);
+	}
+
+}
---
+++ src/test/java/pae/umy/projectktp2/Projectktp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pae.umy.projectktp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projectktp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
